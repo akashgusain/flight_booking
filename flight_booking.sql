2041a5cc04d7c854d181483b38bca10c86aa4c7e
akashgusain <45463611+akashgusain@users.noreply.github.com>
2021-08-24 06:58:44 +0530

    Update flight_booking.sql

--- flight_booking.sql
+++ flight_booking.sql
@@ -1,29 +1,47 @@
 a. List each aircraft type, a separate list of seats is supported.
 
-
-select aircrafts_data.aircraft_code,aircrafts_data.model, s.fare_conditions from aircrafts_data inner join
-    seats s on aircrafts_data.aircraft_code = s.aircraft_code group by model, aircrafts_data.aircraft_code
-, s.fare_conditions order by aircrafts_data.aircraft_code;
+select aircrafts_data.aircraft_code,
+       aircrafts_data.model ->> 'en' as aircraft_model,
+       s.seat_no,
+       s.fare_conditions
+from aircrafts_data
+         inner join
+     seats s on aircrafts_data.aircraft_code = s.aircraft_code
+group by model, aircrafts_data.aircraft_code, s.seat_no,
+         s.fare_conditions
+order by aircrafts_data.aircraft_code, s.seat_no;
 
 
 b. Which type of aircraft has the most travel class?
 
-select ad.model,count(flights.flight_id) from flights inner join
-    aircrafts_data ad on ad.aircraft_code = flights.aircraft_code
-group by ad.model order by count(flights.flight_id) desc;
+select ad.model ->> 'en' as aircraft_model, tf.fare_conditions, count(distinct tf.ticket_no)
+from aircrafts_data ad
+         inner join flights f on ad.aircraft_code = f.aircraft_code
+         inner join ticket_flights tf on f.flight_id = tf.flight_id
+group by ad.model, tf.fare_conditions
+order by aircraft_model, count(distinct tf.ticket_no) desc;
 
 c. Which cities have more than one airport?
 
-select timezone, count(timezone) from airports_data group by timezone
-order by count(timezone) desc;
+select city, count(airport_name)
+from airports
+group by city
+order by count(airport_name) desc;
 
 d. Find the next flight from Yekaterinburg to Moscow.
 
-select scheduled_departure,status  from flights
-where departure_airport='SVX' and arrival_airport = 'SVO'
-and status='Scheduled' order by scheduled_departure;
+select *
+from flights
+where departure_airport = 'SVX'
+  and arrival_airport = 'SVO'
+  and scheduled_departure > bookings.now()
+order by scheduled_departure desc;
 
 e. Find top 5-spending customers.
 
-select passenger_name, count(ticket_no) from tickets group by passenger_name
-order by count(ticket_no) desc limit 5;
+select passenger_name, b.book_ref, sum(b.total_amount)
+from tickets
+         inner join bookings b on b.book_ref = tickets.book_ref
+group by passenger_name, b.book_ref
+order by sum(total_amount) desc
+limit 5;
